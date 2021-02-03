80dca185940de1470c8d0711487dc886373c87c1
MCannon33 <marialcannon@gmail.com>
2021-02-02 23:31:33 -0500

    install inquierer, create form, ignore npm modules

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const profileDataArgs = process.argv.slice(2);
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const inquirer = require("inquirer");
 
-  console.log("================");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
-printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
